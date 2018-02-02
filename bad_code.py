26032fd0a4110820d058b1970a54e99f16e6ef4b
Wilhem Alcivar <wilhemalcivar@Wilhems-MacBook-Pro.local>
2018-02-02 12:44:21 -0500

    added a couple source code files

---
+++ bad_code.py
@@ -0,0 +1,9 @@
+import sys
+import json #I dont use these imports
+
+print "starting project with a bad print statement"
+
+def func(var1):
+	return "this doesnt do anything"
+
+	print('and I have something below the return')
